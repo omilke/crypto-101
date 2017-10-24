73dac42771f1aa30293676fbe1e6ef5082937145
Oliver Milke <oliver.milke@cloudogu.com>
2017-10-24 07:50:47 +0200

    add password value object

---
+++ src/main/java/de/omilke/crypto101/password/Password.java
@@ -0,0 +1,23 @@
+package de.omilke.crypto101.password;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Password {
+
+    private final String hashedPassword;
+
+    public Password(String plainPassword) {
+
+        this.hashedPassword = hashPassword(plainPassword);
+    }
+
+    public boolean matches(String plainPassword){
+
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+
+    private String hashPassword(String plain) {
+
+        return BCrypt.hashpw(plain, BCrypt.gensalt(12));
+    }
+}
---
+++ src/test/java/de/omilke/crypto101/password/PasswordTest.java
@@ -0,0 +1,20 @@
+package de.omilke.crypto101.password;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class PasswordTest {
+
+    @Test
+    public void matches() throws Exception {
+
+        final String examplePassword = "notAGoodPassword";
+
+        Password password = new Password(examplePassword);
+
+
+        assertThat(password.matches(examplePassword)).isTrue();
+        assertThat(password.matches("some-other-pw")).isFalse();
+    }
+
+}
